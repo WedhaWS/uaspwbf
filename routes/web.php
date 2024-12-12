5cb6fda62df0444310db711f857b5ce243547797
Merlin <merlin.lutvika@gmail.com>
2024-12-12 15:40:39 +0700

    Coba coba

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('test');
+});
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/register', [AuthController::class, 'showRegister']);
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+////
